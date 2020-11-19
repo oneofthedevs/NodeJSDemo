ece75eb76a3e5042a0551ccbd2ed6ed7e7d44449
oneofthedevs <iamdevchhaniyara@gmail.com>
2020-11-19 13:00:08 +0530

    :fire: Mail logic sent to separate file

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,15 +1,19 @@
-const postModel = require("../models/models");
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const csurf = require("csurf");
 // const { v4: uuidv4 } = require("uuid");
 
+const postModel = require("../models/models");
+const { sendMail } = require("../Logic/Mail");
+
 // Register
 exports.register = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json();
+    return res
+      .status(400)
+      .json({ status: 400, response: { message: "Some fields are empty" } });
   }
   const { username, email, password } = req.body;
   try {
@@ -25,7 +29,6 @@ exports.register = async (req, res, next) => {
       console.log(user);
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
-      // user.userId = uuidv4();
       user = await user.save();
       const payload = {
         user: user._id,
@@ -35,6 +38,8 @@ exports.register = async (req, res, next) => {
         expiresIn: process.env.ACCESS_TOKEN_LIFE,
         algorithm: "HS256",
       });
+
+      await sendMail(email);
 
       res.status(200).json({ status: 200, response: { token: accessToken } });
     } else {
@@ -104,29 +109,6 @@ exports.login = async (req, res, next) => {
       algorithm: "HS256",
     });
 
-    // jwt.sign(
-    //   payLoad,
-    //   "secret",
-    //   {
-    //     expiresIn: 3600,
-    //     issuer: "DC",
-    //     notBefore: Date.now(),
-    //     algorithm: "HS256",
-    //     subject: "Token",
-    //   },
-    //   (err, token) => {
-    //     if (err) throw err;
-    //     res.setHeader("Set-Cookie", `token = ${token}; Max-Age=30; HttpOnly`);
-    //     // req.session.isLoggedIn = true;
-    //     // res.redirect();
-    //     res.setHeader("Set-Cookie", `token=${token}; HttpOnly; Max-Age=3600`);
-    //     res.status(200).json({
-    //       status: 200,
-    //       response: { token: token },
-    //     });
-    //   }
-    // );
-    // users[username].refreshToken = refreshToken;
     res
       .status(200)
       // .setHeader("Set-Cookie", `jwt=${accessToken}; HttpOnly; Max-Age=3600`)
@@ -148,9 +130,7 @@ exports.getUsers = async (req, res, next) => {
 
 // Get All blogs
 exports.getBlogs = async (req, res, next) => {
-  // const errors = validationResult(req);
   const { id } = req.body;
-  // console.log(req.body, id);
   try {
     if (!id) {
       return res.status(400).json({ status: 400, response: "Hmmmm" });
